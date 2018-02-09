b811142472672bd867a806697496b070d37b3de9
Vidushan <kvidushan@hotmail.com>
2018-02-09 06:16:03 -0800

    added a function for typed.js; set it so it appears on load

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var typed2 = new Typed('.typed', {
-   strings: ["Hey, i'm Vidushan"],
-   typeSpeed: 40,
-   fadeOut: true,
-   loop: false,
- });
+function showType(){
+  var typed2 = new Typed('.typed', {
+     strings: ["Hey, i'm Vidushan"],
+     typeSpeed: 40,
+     fadeOut: true,
+     loop: false,
+   });
 
- var typed2 = new Typed('.subtitle', {
-    strings: ["I'm Full-Stack Web Developer, based in Vancouver, BC!", 'I love developing sites using various programming languages!', 'I love figuring out ways to improve user experience!'],
-    typeSpeed: 60,
-    fadeOut: true,
-    loop: true,
-    cursorChar: '_',
- })
+   var typed2 = new Typed('.subtitle', {
+      strings: ["I'm Full-Stack Web Developer, based in Vancouver, BC!", 'I love developing sites using various programming languages!', 'I love figuring out ways to improve user experience!'],
+      typeSpeed: 60,
+      fadeOut: true,
+      loop: true,
+      cursorChar: '_',
+   })
+}
